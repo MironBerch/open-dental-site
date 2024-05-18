4376c9e3d29c768e8395862f65a63dcae39699f1
MironBerch <mironberchetov@gmail.com>
2024-05-18 19:28:00 +0300

    Update update.sh

--- update.sh
+++ update.sh
@@ -3,7 +3,7 @@
 docker stop $(docker ps -a -q) && docker rm $(docker ps -a -q)
 
 docker volume rm open-dental-site_static_volume
+docker volume ls --format '{{.Name}}' | grep -E '^[a-f0-9]{64}$' | xargs -r docker volume rm
 
 docker pull $DOCKERHUB_USERNAME/dental-nginx:latest
 docker pull $DOCKERHUB_USERNAME/dental:latest
-
