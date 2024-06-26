1003865ed96cbac445213367e16143c73668b5e4
MironBerch <mironberchetov@gmail.com>
2024-06-26 17:28:47 +0300

    Update script for init test data

--- init-data.sh
+++ init-data.sh
@@ -1,11 +1,19 @@
 #!/bin/sh
 
-cp --update=none "./conf/data/"* "./src/"
+cp -u "./conf/data/"* "./src/"
 cp --recursive "./conf/data/media" "./src/"
 
+cd src/
+
 uv run manage.py loaddata staff.json && \
-uv run manage.py loaddata works.json && \
-uv run manage.py loaddata reviews.json && \
-uv run manage.py loaddata prices.json && \
-uv run manage.py loaddata pricegroups.json && \
-uv run manage.py loaddata contacts.json
+    uv run manage.py loaddata works.json && \
+    uv run manage.py loaddata reviews.json && \
+    uv run manage.py loaddata pricegroups.json && \
+    uv run manage.py loaddata prices.json && \
+    uv run manage.py loaddata contacts.json && \
+    uv run manage.py loaddata servicegroups.json && \
+    uv run manage.py loaddata services.json && \
+    uv run manage.py loaddata about.json && \
+    uv run manage.py loaddata policy.json
+
+cd ..
