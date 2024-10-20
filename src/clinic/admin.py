4023e31816bba4a2d4628b2c9e5e0ec6a04c0283
MironBerch <mironberchetov@gmail.com>
2024-10-20 22:47:38 +0300

    Добавить панель администратора

---
+++ src/clinic/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+
+from clinic.models import License, About, Details
+
+admin.site.register(License)
+admin.site.register(About)
+admin.site.register(Details)
---
+++ src/staff/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+
+from staff.models import Role, Staff
+
+admin.site.register(Role)
+admin.site.register(Staff)
