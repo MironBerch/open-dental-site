a967cf2e4ef56dc8aec467a23d2d32fb667bf19d
MironBerch <mironberchetov@gmail.com>
2024-10-26 20:37:14 +0300

    Создана возможность оставлять отзывы

---
+++ src/reviews/forms.py
@@ -0,0 +1,40 @@
+from django import forms
+from django.core.validators import RegexValidator
+from phonenumber_field.formfields import PhoneNumberField
+
+from reviews.models import Review
+
+
+class ReviewForm(forms.ModelForm):
+    phone = PhoneNumberField(
+        label='Номер телефона',
+        validators=[
+            RegexValidator(
+                regex=r'^\+?[0-9]{7,15}$',
+                message='Номер телефона необходимо ввести в формате: +XXXXXXXXXXXXX.',
+            ),
+        ],
+        required=False,
+    )
+
+    class Meta:
+        model = Review
+        fields = ('name', 'email', 'phone', 'message', 'rating', 'image')
+        widgets = {
+            'name': forms.TextInput(attrs={'placeholder': ''}),
+            'email': forms.EmailInput(attrs={'placeholder': ''}),
+            'phone': forms.TextInput(attrs={'placeholder': ''}),
+            'message': forms.Textarea(attrs={'rows': 4, 'placeholder': ''}),
+            'rating': forms.Select(
+                choices=[(i, i) for i in range(1, 6)],
+                attrs={'class': 'rating-select'},
+            ),
+            'image': forms.ClearableFileInput(attrs={'placeholder': ''}),
+        }
+        labels = {
+            'name': 'Ваше имя*',
+            'email': 'Почта*',
+            'message': 'Сообщение*',
+            'rating': 'Оценка*',
+            'image': 'Фото',
+        }
--- src/reviews/views.py
+++ src/reviews/views.py
@@ -1,7 +1,10 @@
+from django.contrib import messages
 from django.http import HttpRequest
+from django.shortcuts import redirect
 from django.views.generic import View
 from django.views.generic.base import TemplateResponseMixin
 
+from reviews.forms import ReviewForm
 from reviews.services import get_published_reviews
 
 #  from django.core.paginator import Paginator
@@ -9,6 +12,7 @@ from reviews.services import get_published_reviews
 
 class ReviewsView(TemplateResponseMixin, View):
     template_name = 'reviews/view.html'
+    form_class = ReviewForm
     #  paginate_by = 10
 
     def get(self, request: HttpRequest, *args, **kwargs):
@@ -18,6 +22,25 @@ class ReviewsView(TemplateResponseMixin, View):
         return self.render_to_response(
             context={
                 #  'page_object': page_object,
+                'form': self.form_class(),
+                'active_page': 'reviews',
+                'reviews': get_published_reviews().order_by('-id'),
+            },
+        )
+
+    def post(self, request: HttpRequest, *args, **kwargs):
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            messages.add_message(
+                request,
+                messages.SUCCESS,
+                'Отзыв успешно оставлен',
+            )
+            form.save()
+            return redirect('reviews')
+        return self.render_to_response(
+            context={
+                'form': self.form_class,
                 'active_page': 'reviews',
                 'reviews': get_published_reviews().order_by('-id'),
             },
