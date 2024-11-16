d73d4feb1bf33e865febefb40f397bf455e625aa
MironBerch <mironberchetov@gmail.com>
2024-11-17 01:29:00 +0300

    Move urls to config urls file

--- src/config/urls.py
+++ src/config/urls.py
@@ -5,15 +5,107 @@ from django.urls import include, path
 
 from debug_toolbar.toolbar import debug_toolbar_urls
 
+from clinic.views import AboutView, LicensesView, RequisitesView, ContactsView, LicenseView
+from works.views import WorksView, WorkView
+from main.views import SitemapView, MainView, SearchAPIView, SearchView
+from prices.views import PricesView
+from reviews.views import ReviewsView
+from services.views import ServicesGroupsView, ServicesGroupView, ServiceView
+from staff.views import StaffView, StaffDetailView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('main.urls')),
-    path('', include('clinic.urls')),
-    path('', include('services.urls')),
-    path('', include('staff.urls')),
-    path('', include('works.urls')),
-    path('', include('prices.urls')),
-    path('reviews/', include('reviews.urls')),
+
+    # Gallery
+    path('gallery/', WorksView.as_view(), name='gallery'),
+    path('gallery/<slug>/', WorkView.as_view(), name='work'),
+
+    # Services
+    path(
+        route='services/',
+        view=ServicesGroupsView.as_view(),
+        name='services_groups',
+    ),
+    path(
+        route='services/<slug>/',
+        view=ServicesGroupView.as_view(),
+        name='services_group',
+    ),
+    path(
+        route='service/<slug>/',
+        view=ServiceView.as_view(),
+        name='service',
+    ),
+    path(
+        route='price/',
+        view=PricesView.as_view(),
+        name='prices',
+    ),
+
+    path(
+        route='sitemap/',
+        view=SitemapView.as_view(),
+        name='site_map',
+    ),
+    path(
+        route='',
+        view=MainView.as_view(),
+        name='main',
+    ),
+
+    # Search
+    path(
+        route='api/search/',
+        view=SearchAPIView.as_view(),
+        name='search',
+    ),
+    path(
+        route='search/',
+        view=SearchView.as_view(),
+        name='search_view',
+    ),
+
+    # Company
+    path(
+        route='company/requisites/',
+        view=RequisitesView.as_view(),
+        name='requisites',
+    ),
+    path(
+        route='company/',
+        view=AboutView.as_view(),
+        name='company',
+    ),
+    path(
+        route='company/licenses/',
+        view=LicensesView.as_view(),
+        name='licenses',
+    ),
+    path(
+        route='company/licenses/<slug>/',
+        view=LicenseView.as_view(),
+        name='license',
+    ),
+    path(
+        route='company/contacts/',
+        view=ContactsView.as_view(),
+        name='contacts',
+    ),
+    path(
+        route='company/reviews/',
+        view=ReviewsView.as_view(),
+        name='reviews',
+    ),
+    path(
+        route='company/staff/',
+        view=StaffView.as_view(),
+        name='staff_list',
+    ),
+    path(
+        route='company/staff/<slug>/',
+        view=StaffDetailView.as_view(),
+        name='staff',
+    ),
 
     path('ckeditor5/', include('django_ckeditor_5.urls')),
 ] + debug_toolbar_urls()
--- src/staff/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from staff.views import StaffView, StaffDetailView
-
-urlpatterns = [
-    path('staff/', StaffView.as_view(), name='staff_list'),
-    path('staff/<slug>/', StaffDetailView.as_view(), name='staff'),
-]
