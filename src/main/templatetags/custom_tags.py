eab7c1e4cec9b7517e95f176bba6edc007c38657
MironBerch <mironberchetov@gmail.com>
2025-04-12 16:12:43 +0300

    Update

--- src/main/templatetags/custom_tags.py
+++ src/main/templatetags/custom_tags.py
@@ -4,17 +4,6 @@ register = template.Library()
 
 
 @register.filter
-def map_stage(stage):
-    stage_mapping = {
-        'Руководитель': 'rukovoditeli',
-        'Администратор': 'administratori',
-        'Мед персонал': 'mediki',
-        'Младщий мед персонал': 'assistenti'
-    }
-    return stage_mapping.get(stage, '')
-
-
-@register.filter
 def rubles_formatted(value):
     try:
         value = int(value)
