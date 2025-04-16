adf19b2a9581f1e63d2faf5b3ed1d53d07e7596d
MironBerch <mironberchetov@gmail.com>
2025-04-16 20:19:02 +0300

    Update docker compose, deploy script and create nginx image
    Remove docker-compose-production.yml
    Update docker-compose-prod.yml
    Update deploy-image-to-docker.sh
    Create nginx image

--- deploy-image-to-docker.sh
+++ deploy-image-to-docker.sh
@@ -5,3 +5,12 @@ echo "$DOCKERHUB_TOKEN" | docker login -u $DOCKER_USERNAME --password-stdin
 docker build -t $DOCKERHUB_USERNAME/dental:latest .
 
 docker push $DOCKERHUB_USERNAME/dental:latest
+
+
+cd conf/nginx/
+
+docker build -t $DOCKERHUB_USERNAME/dental-nginx:latest .
+
+docker push $DOCKERHUB_USERNAME/dental-nginx:latest
+
+cd ../..
