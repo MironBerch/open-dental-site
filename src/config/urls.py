236d120b1933ff7a225ef1bffa8c0778496252e0
MironBerch <mironberchetov@gmail.com>
2025-05-16 14:36:01 +0300

    Add robots.txt

--- src/config/urls.py
+++ src/config/urls.py
@@ -5,6 +5,7 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.contrib.sitemaps.views import sitemap
 from django.urls import include, path
+from django.views.generic.base import TemplateView
 
 from clinic.views import (
     AboutView,
@@ -137,6 +138,14 @@ urlpatterns = [
             'sitemaps': sitemaps,
         },
         name='django.contrib.sitemaps.views.sitemap',
+    ),
+
+    path(
+        'robots.txt',
+        TemplateView.as_view(
+            template_name='robots.txt',
+            content_type='text/plain',
+        ),
     ),
 
     path('ckeditor5/', include('django_ckeditor_5.urls')),
